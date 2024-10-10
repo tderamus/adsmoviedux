e6e7c9ed410372c4b31fd5a5344a0ca7840a5b8a
tderamus <44751022+tderamus@users.noreply.github.com>
2024-10-10 17:40:12 -0500

    setup project and configured package.json dependencies. Setup App component to render home page

---
+++ adsmoviesreact/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import './styles.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="header">
+        <h1>Welcome to MovieDux</h1>
+      </header>
+      <footer className="footer">
+        <p className="footer">Footer content here</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
