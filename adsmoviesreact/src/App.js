9ba2d167625f534040dc35157b02b79fac266a04
tderamus <44751022+tderamus@users.noreply.github.com>
2024-10-10 19:59:42 -0500

    added mapped data from json data file, mapped over data by ID and added movie data props to the card tags

--- adsmoviesreact/src/App.js
+++ adsmoviesreact/src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import './styles.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import MoviesGrid from './components/MoviesGrid';
 
 function App() {
   return (
     <div className="App">
       <div className='container'>
       <Header/>
+      <MoviesGrid></MoviesGrid>
       </div>
       <Footer></Footer>
     </div>
